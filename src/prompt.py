2a796300411c52d8029f25e1e40bf2f3b59fff0c
Alok-Kumar2005 <ay747283@gmail.com>
2025-01-03 22:24:54 +0530

    app created

---
+++ src/prompt.py
@@ -0,0 +1,11 @@
+
+system_prompt = (
+    "You are an assistant for question answering task"
+    "use the following piece of retrieved context to answer"
+    "the question, if you dont know the answer, say that you"
+    "dont know , Use three sentences maximun and keep up"
+    "answer concise"
+    "\n\n"
+    "{context}"
+)
+
